fe481e0eef858a49bb3189f6df0201b9dd35b912
Noxmore <noxmore@noxmore.dev>
2025-06-17 19:48:00 -0700

    More work

--- nil/bserial.h
+++ nil/bserial.h
@@ -47,15 +47,76 @@ typedef struct Foo {
 	Vec(u16) baz;
 } Foo;
 
+/*typedef void (*SerializeFunction)(FILE*, const void*);
+typedef bool (*DeserializeFunction)(FILE*, void*);
+
+typedef struct SerialFunctions {
+
+} SerialFunctions;*/
 
 #define binSerialize(T) T##_binSerialize
 #define binDeserialize(T) T##_binDeserialize
 
-#define BIN_SERIAL
+#define BIN_SERIAL_INTERNAL(T, SERIALIZE, DESERIALIZE) \
+	static void binSerialize(T)(FILE* dst, const T* v) { \
+		const u16 size = sizeof(T); \
+		binSerialize(u16)(dst, &size); \
+		SERIALIZE \
+	} \
+	static bool binDeserialize(T)(FILE* src, T* dst) { \
+		u16 size; \
+		if (!binDeserialize(u16)(src, &size)) return false; \
+		T v = {0}; \
+		DESERIALIZE \
+		return true; \
+	} \
+	// BIN_SERIAL_DEFINE_CONTAINERS(T)
+// TODO: defaults, error propagation, and size limiting
+
+#define BIN_SERIAL_INTERNAL_GET_OVERRIDE(_1, _1b, _2, _2b, _3, _3b, pad, NAME, ...) NAME
+
+#define BIN_SERIAL(T, ...) BIN_SERIAL_INTERNAL_GET_OVERRIDE(__VA_ARGS__, BIN_SERIAL_3, invalid3, BIN_SERIAL2, invalid2, BIN_SERIAL1, invalid1)(T, __VA_ARGS__)
+
+#define BIN_SERIAL1(T, T0, F0) BIN_SERIAL_INTERNAL(T, \
+	binSerialize(T0)(dst, &v->F0);,\
+	if (!binDeserialize(T0)(src, &v.F0)) return false;\
+)
+
+#define BIN_SERIAL2(T, T0, F0, T1, F1) BIN_SERIAL_INTERNAL(T, \
+	{ \
+		binSerialize(T0)(dst, &v->F0); \
+		binSerialize(T1)(dst, &v->F1); \
+	}, \
+	{ \
+		if (!binDeserialize(T0)(src, &v.F0)) return false; \
+		if (!binDeserialize(T1)(src, &v.F1)) return false; \
+	} \
+)
+
+
+// #define BIN_SERIAL_FIELD(T, FIELD) (T)(&v->FIELD)
+
+#define BIN_SERIAL_DEFINE_CONTAINERS(T) \
+	static void binSerialize(Vec$##T)(FILE* dst, const Vec(T)* v) { for (usize i = 0; i < v->len; i++) binSerialize(T)(dst, &v->data[i]); } \
+	static inline void binSerialize(Box$##T)(FILE* dst, const T** v) { binSerialize(T)(dst, *v); } \
+	static bool binDeserialize(Vec$##T)(FILE* src, Vec(T)* dst) { \
+		if (!binDeserialize(usize)(src, &dst->len)) return false; \
+		dst->cap = dst->len; \
+		dst->data = nullptr; \
+		vecReserve(dst, dst->len); \
+		for (usize i = 0; i < dst->len; i++) \
+			if (!binDeserialize(T)(src, vecReserveItem(dst))) return false; \
+		return true; \
+	} \
+	static inline bool binDeserialize(Box$##T)(FILE* src, T** dst) { return binDeserialize(T)(src, *dst); }
 
 #define BIN_SERIAL_STABLE_SIMPLE(T) \
 	static inline void T##_binSerialize(FILE* dst, const T* v) { fwrite(v, sizeof(T), 1, dst); } \
-	static inline bool T##_binDeserialize(FILE* src, T* dst) { return fread(&dst, sizeof(T), 1, src) < sizeof(T); }
+	static inline bool T##_binDeserialize(FILE* src, T* dst) { return fread(dst, sizeof(T), 1, src) < sizeof(T); } \
+	BIN_SERIAL_DEFINE_CONTAINERS(T)
+
+
+BIN_SERIAL_STABLE_SIMPLE(usize)
 
 BIN_SERIAL_STABLE_SIMPLE(i8)
 BIN_SERIAL_STABLE_SIMPLE(i16)
@@ -67,9 +128,23 @@ BIN_SERIAL_STABLE_SIMPLE(u16)
 BIN_SERIAL_STABLE_SIMPLE(u32)
 BIN_SERIAL_STABLE_SIMPLE(u64)
 
-BIN_SERIAL_STABLE_SIMPLE(usize)
-
 BIN_SERIAL_STABLE_SIMPLE(float)
 BIN_SERIAL_STABLE_SIMPLE(double)
 
-BIN_SERIAL_STABLE_SIMPLE(Foo)
+// BIN_SERIAL_STABLE_SIMPLE(Foo)
+
+BIN_SERIAL(Foo,
+	i32, bap,
+	float, bar,
+	// Vec$u16, baz,
+)
+
+void fooTest(FILE* dst, const Foo* v) {
+	// _GET_OVERRIDE(a, b, o3, o2, o0);
+	u16 size = sizeof(Foo);
+	fwrite(&size, sizeof(u16), 1, dst);
+
+	binSerialize(i32)(dst, &v->bap);
+	binSerialize(float)(dst, &v->bar);
+	binSerialize(Vec$u16)(dst, &v->baz);
+}
--- test.c
+++ test.c
@@ -14,7 +14,7 @@ int main() {
 
 	FILE* file = fopen("out.bin", "w");
 	if (file == nullptr) {
-		printf("Failed to open file");
+		printf("Failed to open file\n");
 		return 1;
 	}
 
@@ -24,25 +24,26 @@ int main() {
 
 	fclose(file);
 
-	printf("Serialized");
+	printf("Serialized\n");
 
 	file = fopen("out.bin", "r");
 	if (file == nullptr) {
-		printf("Failed to open file for reading");
+		printf("Failed to open file for reading\n");
 		return 1;
 	}
 
 	Foo foo2;
+	// fread(&foo2, sizeof(Foo), 1, file);
 	if (!Foo_binDeserialize(file, &foo2)) {
-		printf("Failed to deserialize");
+		printf("Failed to deserialize\n");
 		return 1;
 	}
 
 	fclose(file);
 
 	if (memcmp(&foo, &foo2, sizeof(Foo)) == 0) {
-		printf("Sucess");
+		printf("Sucess\n");
 	} else {
-		printf("Failed! Deserialized foo does not match");
+		printf("Failed! Deserialized foo does not match\n");
 	}
 }
